960580f7a73d4e7e0d87fef7181a15d315bd747f
nobumitsu-1995 <70850598+nobumitsu-1995@users.noreply.github.com>
2022-04-05 03:18:27 +0900

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "https://todo-next-rails-backend.herokuapp.com",
+  baseURL: "https://next-rails-todo-backend.herokuapp.com",
   responseType: "json",
   headers: {
     "Content-Type": "application/json",
